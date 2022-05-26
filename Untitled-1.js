6999d09bfef9f7b26828e50419aa2690f0261934
MorgPC <heroinamagazine@gmail.com>
2022-05-25 23:36:39 -0500

    welrjkalsdf
    lajsdlfjas

---
+++ Untitled-1.js
@@ -0,0 +1,20 @@
+// console.log(5);
+
+// let number = 0;
+// while (number <= 12) {
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// let result = 1, count = 0;
+// while (count < 10) {
+//     result = result * 2;
+//     count = count + 1;
+// }
+// console.log(result);
+
+let yourName;
+do {
+    yourName = "Morgan"
+} while (!yourName);
+console.log(yourName);
