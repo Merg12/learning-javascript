3d6bc568ea9d3fac997824cb12f6c675070945a0
MorgPC <heroinamagazine@gmail.com>
2022-06-05 14:02:31 -0500

    just committing

--- Untitled-1.js
+++ Untitled-1.js
@@ -70,13 +70,13 @@
 //         break;
 // }
 
-for (let count = 0; count <= 100; count++) {
-    if (count % 3 == 0 && count % 5 == 0) {
-        console.log("FizzBuzz");
-    } else if (count % 3 == 0) {
-        console.log("Fizz");
-    } else if (count % 5 == 0) {
-        console.log("Buzz");
-    } else {console.log(count);}
+// for (let count = 0; count <= 100; count++) {
+//     if (count % 3 == 0 && count % 5 == 0) {
+//         console.log("FizzBuzz");
+//     } else if (count % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (count % 5 == 0) {
+//         console.log("Buzz");
+//     } else {console.log(count);}
         
-    }
+//     }
