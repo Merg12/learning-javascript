48cbabfa14bfa96c4ccffc8ac78a654f7f5735d5
MorgPC <heroinamagazine@gmail.com>
2022-06-06 01:02:06 -0500

    al;sdjf

---
+++ growing-functions-p52.js
@@ -0,0 +1,14 @@
+function printFarmInventory(cows, chickens) {
+    let cowString = String(cows);
+    while (cowString.length < 3) {
+        cowString = "0" + cowString;
+    }
+    console.log(`${cowString} Cows`);
+
+    let chickensString = String(chickens);
+    while (chickensString.length < 3) {
+        chickensString = "0" + chickensString;
+    }
+    console.log(`${chickensString} Chickens`);
+}
+printFarmInventory(2, 54)
--- recursion-p50.js
+++ recursion-p50.js
@@ -18,5 +18,41 @@ console.log(findSolution(4));
 so line 8 return is pretty wild, there's the || operator and on each side there's another function call
 so how does it work?
 
+If the current number
+is the target number, the current history is a way to reach that target, so it
+is returned. If the current number is greater than the target, there’s no sense
+in further exploring this branch because both adding and multiplying will only
+make the number bigger, so it returns null. Finally, if we’re still below the
+target number, the function tries both possible paths that start from the current
+number by calling itself twice, once for addition and once for multiplication. If
+the first call returns something that is not null, it is returned. Otherwise, the
+second call is returned, regardless of whether it produces a string or null.
+To better understand how this function produces the effect we’re looking for,
+let’s look at all the calls to find that are made when searching for a solution
+for the number 13.
 
+find(1, "1")
+    find(6, "(1 + 5)")
+        find(11, "((1 + 5) + 5)")
+            find(16, "(((1 + 5) + 5) + 5)")
+                too big
+            find(33, "(((1 + 5) + 5) * 3)")
+                too big
+        find(18, "((1 + 5) * 3)")
+            too big
+    find(3, "(1 * 3)")
+        find(8, "((1 * 3) + 5)")
+            find(13, "(((1 * 3) + 5) + 5)")
+                found!
+
+The indentation indicates the depth of the call stack. The first time find is
+called, it starts by calling itself to explore the solution that starts with (1 + 5).
+That call will further recurse to explore every continued solution that yields a
+number less than or equal to the target number. Since it doesn’t find one that
+hits the target, it returns null back to the first call. There the || operator
+causes the call that explores (1 * 3) to happen. This search has more luck—
+its first recursive call, through yet another recursive call, hits upon the target
+number. That innermost call returns a string, and each of the || operators
+in the intermediate calls passes that string along, ultimately returning the
+solution.
 */
