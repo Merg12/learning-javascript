e8d429304e39d9e228701870a64d62a949ca9b71
Morg <heroinamagazine@gmail.com>
2022-08-06 22:16:29 -0500

    ljsdlfk

--- test2.js
+++ test2.js
@@ -1,13 +1,8 @@
-//generate a pyramide of $
-var n = 10;
-var str = "";
-for (var i = 0; i < n; i++) {
-    for (var j = 0; j < n - i; j++) {
-        str += "";
-    }
-    for (var k = 0; k <= i; k++) {
-        str += "$";
-    }
-    str += "\n";
+//generate a triangle of #
+for (var i = 1; i <= 7; i++) {
+  var str = '';
+  for (var j = 1; j <= i; j++) {
+    str += '#';
+  }
+  console.log(str);
 }
-console.log(str);
